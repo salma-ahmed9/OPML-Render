58b677c878b489668dc83d0edf69bed3add05e34
salma-ahmed9 <93517375+salma-ahmed9@users.noreply.github.com>
2023-07-07 04:04:54 +0200

    code added

---
+++ DealingWithOPML/Pages/Feed.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+
+namespace DealingWithOPML.Pages
+{
+    public class FeedModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public FeedModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public List<RssItem> RssItemsList { get; private set; } = new();
+        public async Task<List<RssItem>> FetchParse(string feedLink)
+        {
+            List<RssItem> rssList = new List<RssItem>();
+            HttpClient httpClient = _clientFactory.CreateClient();
+            HttpResponseMessage httpResponse = await httpClient.GetAsync(feedLink);
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                HttpContent responseContent = httpResponse.Content;
+                string responseData = await responseContent.ReadAsStringAsync();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(responseData);
+                XmlNodeList? nodes = doc.DocumentElement?.SelectNodes("//item");
+                foreach (XmlNode node in nodes)
+                {
+                    RssItem rssObject = new RssItem();
+
+                    string description = (node.SelectSingleNode("description") == null) ? "" : node.SelectSingleNode("description").InnerText;
+                    HtmlString htmlDescription = new HtmlString(description);
+                    rssObject.Description = htmlDescription;
+                    rssObject.Title = (node.SelectSingleNode("title") == null) ? "" : node.SelectSingleNode("title").InnerText;
+                    rssObject.Link = (node.SelectSingleNode("link") == null) ? "" : node.SelectSingleNode("link").InnerText;
+                    rssObject.PublishDate = (node.SelectSingleNode("pubDate") == null) ? "" : node.SelectSingleNode("pubDate").InnerText;
+                    rssList.Add(rssObject);
+                }
+            }
+            httpClient.Dispose();
+            return rssList;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            string feedLink = Request.Query["message"];
+            var myRssList = await FetchParse(feedLink);
+            RssItemsList = myRssList;
+            return Page();
+        }
+    }
+    public class RssItem
+    {
+        public string? Title { get; set; }
+        public HtmlString? Description { get; set; }
+        public string? Link { get; set; }
+        public string? PublishDate { get; set; }
+    }
+}
---
+++ DealingWithOPML/Pages/Index.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+
+
+namespace DealingWithOPML.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public List<RssFeed> RssFeedList { get; private set; } = new();
+        public int CurrentPage { get; private set; }
+
+        public async Task<List<RssFeed>> FetchParse()
+        {
+            List<RssFeed> feedList = new List<RssFeed>();
+            HttpClient httpClient = _clientFactory.CreateClient();
+            HttpResponseMessage httpResponse = await httpClient.GetAsync("https://blue.feedland.org/opml?screenname=dave");
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                HttpContent responseContent = httpResponse.Content;
+                string responseData = await responseContent.ReadAsStringAsync();
+                Console.WriteLine(responseData);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(responseData);
+                XmlNodeList? nodes = doc.DocumentElement?.GetElementsByTagName("outline");
+
+                foreach (XmlNode node in nodes)
+                {
+                    RssFeed feedObject = new RssFeed();
+                    feedObject.FeedTitle = node.Attributes?["text"]?.Value;
+                    feedObject.FeedLink = node.Attributes?["xmlUrl"]?.Value;
+                    feedList.Add(feedObject);
+                }
+            }
+            httpClient.Dispose();
+            return feedList;
+        }
+        public async Task<IActionResult> OnGetAsync(int pageNumber = 1, int itemsPerPage = 5)
+        {
+            var myFeedList = await FetchParse();
+            int startIndex = (pageNumber - 1) * itemsPerPage;
+            int totalItems = myFeedList.Count;
+            int totalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage);
+            if (startIndex >= totalItems)
+            {
+                startIndex = (totalPages - 1) * itemsPerPage;
+                pageNumber = totalPages;
+            }
+            RssFeedList = myFeedList.GetRange(startIndex, Math.Min(itemsPerPage, totalItems - startIndex));
+            CurrentPage = pageNumber;
+            ViewData["TotalPages"] = totalPages;
+            return Page();
+        }
+    }
+    public class RssFeed
+    {
+        public string? FeedTitle { get; set; }
+        public string? FeedLink { get; set; }
+    }
+
+
+
+}
